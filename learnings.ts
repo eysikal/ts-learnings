820e541df6d7cceb8b77f453926f63f8f14d9647
eysikal <benjamin.russell@lendio.com>
2024-12-23 19:54:42 -0700

    Add on

--- learnings.ts
+++ learnings.ts
@@ -1,1 +1,12 @@
-// You should always configure TS with the noImplicitAny and strictNullChecks options.
+// You should always configure TS with the "noImplicitAny" and "strictNullChecks" options.
+// And if you can, aim to enable the "strict" option.
+// e.g.
+/*
+ * tsconfig.json
+ *
+ * "compilerOptions": {
+ *   "noImplicitAny": true,
+ *   "strictNullChecks": true,
+ * }
+ *
+ */
